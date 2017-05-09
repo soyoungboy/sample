ff1aae7a17346749226bb073330f63f3a54f8941
wangfubin <fubin.wang@quncaotech.com>
2017-05-09 18:31:19 +0800

    HiveProgressView  demo

---
+++ app/src/main/java/com/soyoungboy/sample/ui/activity/HiveProgressViewActivity.java
@@ -0,0 +1,17 @@
+package com.soyoungboy.sample.ui.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import com.soyoungboy.sample.R;
+
+/**
+ * Created by Administrator on 2017/5/9.
+ */
+
+public class HiveProgressViewActivity extends Activity {
+    @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_overwatchprogress);
+    }
+}
